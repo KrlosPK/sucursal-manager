c2d70b76089280534102c8690da4ec3f390a719f
KrlosPK <centille095@gmail.com>
2024-02-07 13:49:05 -0500

    fix eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Navbar } from '@/components/organisms/Navbar'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Hello World</h2>
+    <main>
+      <Navbar />
     </main>
   )
 }
